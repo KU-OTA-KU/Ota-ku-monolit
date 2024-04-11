7c059bdc2cc7410ca4ca658e5c1bb1992c77a71a
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-04-11 19:53:29 +0400

    add scroll to anchor system and fix bug witch watching and mobile search and search placeholder changed
    Signed-off-by: Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>

--- src/components/header/header.php
+++ src/components/header/header.php
@@ -1,67 +1,62 @@
 <!-- header Start -->
 <header>
-  <div class="header-inner">
-    <div class="header-tit">
-      <div class="header-log-and-nav">
-        <div class="header-logo" onclick="window.location.href = 'index.php';">
-          <img src="" alt="" id="logo" />
-        </div>
-        <nav class="header-navigation">
-          <a href="catalog.php" class="nav-button">КАТАЛОГ</a>
-          <a href="#" class="nav-button">ТОП</a>
-        </nav>
-        <div class="header-search">
-          <form action="catalog.php" method="GET">
-            <button type="submit">
-              <i class="fa-solid fa-magnifying-glass"></i>
-            </button>
-            <input type="text" required placeholder="Что мы ищем, Сенпай?" class="search-input-fa" name="search" />
-          </form>
-          <!-- search result Start -->
-          <div class="search-results"></div>
-          <!-- search result End -->
-        </div>
-      </div>
-      <div class="header-login">
-        <div class="user-is-reg-1" style="display: none">
-          <div class="user">
-            <div class="user-cont">
-              <p class="user-name">Esudesu</p>
-              <span class="user-email">w33bv.gl@gmail.com</span>
+    <div class="header-inner">
+        <div class="header-tit">
+            <div class="header-log-and-nav">
+                <div class="header-logo" onclick="window.location.href = 'index.php';">
+                    <img src="" alt="" id="logo"/>
+                </div>
+                <nav class="header-navigation">
+                    <a href="catalog.php" class="nav-button">КАТАЛОГ</a>
+                    <a href="#" class="nav-button">ТОП</a>
+                </nav>
+                <div class="header-search">
+                    <form action="catalog.php" method="GET">
+                        <button type="submit">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                        <input type="text" required placeholder="Что будем искать в мире аниме?" class="search-input-fa"
+                               name="search" autocomplete="off"/>
+                    </form>
+                </div>
             </div>
-            <div class="user-img" onclick="userProfile();">
-              <img src="assets/images/user.jpg" alt="userimg" />
+            <div class="header-login">
+                <div class="user-is-reg-1" style="display: none">
+                    <div class="user">
+                        <div class="user-cont">
+                            <p class="user-name">Esudesu</p>
+                            <span class="user-email">w33bv.gl@gmail.com</span>
+                        </div>
+                        <div class="user-img" onclick="userProfile();">
+                            <img src="assets/images/user.jpg" alt="userimg"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="user-is-reg-2" style="display: flex">
+                    <button onclick="">
+                        Войти
+                    </button>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="user-is-reg-2" style="display: flex">
-          <button onclick="">
-          Войти
-          </button>
-        </div>
-      </div>
     </div>
-  </div>
 </header>
 <!-- header End -->
 
 <!-- search Start media 630px -->
 <section class="search">
-  <div class="search-inner">
-    <div class="search-tit">
-      <div class="header-search spm-search">
-        <form action="#">
-          <button type="submit">
-          <i class="fa-solid fa-magnifying-glass"></i>
-          </button>
-          <input type="text" required placeholder="Что мы ищем, Сенпай?" class="search-input-fa" />
-        </form>
-
-        <!-- search result Start -->
-        <div class="search-results"></div>
-        <!-- search result End -->
-      </div>
+    <div class="search-inner">
+        <div class="search-tit">
+            <div class="header-search spm-search">
+                <form action="catalog.php" method="GET">
+                    <button type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    <input type="text" required placeholder="Что будем искать в мире аниме?" class="search-input-fa"
+                           name="search" autocomplete= "off"/>
+                </form>
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 <!-- search End media 630px -->
