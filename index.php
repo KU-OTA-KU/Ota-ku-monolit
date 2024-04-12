b8d2b54792e1ed99f61a255130b6aa73ec385056
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-04-12 15:03:37 +0400

    fix bug полностю изменили работу скролина топ жанров сделли боле плавным и устранили баг где мы при свайпе в пол свайпа был клик по входу так же сдалал скролинг бесконечным и оптимизировал запросы получение ссылок
    Signed-off-by: Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
 <html lang="ru">
 
 <head>
+    <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+    <script src="https://kit.fontawesome.com/36abf4b57f.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.2/anime.min.js" integrity="sha512-aNMyYYxdIxIaot0Y1/PLuEu3eipGCmsEUBrUq+7aVyPGMFH8z0eTP0tkqAvv34fzN6z+201d3T8HPb1svWSKHQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com"  crossorigin="anonymous">
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>AnimeCO Смотреть аниме Онлайн</title>
@@ -12,10 +19,6 @@
     <link rel="stylesheet" href="src/_root/root.css"/>
     <link rel="stylesheet" href="css/palette_dark.css"/>
     <link rel="stylesheet" href="css/scrollbar.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <link rel="stylesheet" href="src/components/header/header.css"/>
     <link rel="stylesheet" href="css/main.css"/>
     <link rel="stylesheet" href="src/components/_filter/filter.css"/>
@@ -67,11 +70,10 @@
 <!-- footer Init End -->
 
 <!-- scripts -->
-<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-<script src="https://kit.fontawesome.com/36abf4b57f.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.2/anime.min.js" integrity="sha512-aNMyYYxdIxIaot0Y1/PLuEu3eipGCmsEUBrUq+7aVyPGMFH8z0eTP0tkqAvv34fzN6z+201d3T8HPb1svWSKHQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="js/other/delay.js"></script>
 <script src="js/_BLACKLIST.js"></script>
+<script src="js/other/delay.js"></script>
+<script src="js/flickity/genres.js"></script>
+<script src="js/topSlider/isSelected.js"></script>
 <script src="js/genAnimeListSkeleton.js"></script>
 <script src="js/other/shuffleArray.js"></script>
 <script src="js/topSlider/displayAnimeListInFlickity.js"></script>
@@ -82,9 +84,9 @@
 <script src="js/popular/popularAnimes.js"></script>
 <script src="js/animeList.js"></script>
 <script src="js/logo.js"></script>
-<script src="js/most/displayAnimeListMost.js"></script>
-<script src="js/most/mostAnimes.js"></script>
+<script defer src="js/most/displayAnimeListMost.js"></script>
+<script defer src="js/most/mostAnimes.js"></script>
 <script src="js/topSlider/flickityTopAnimes.js"></script>
-<script src="js/topSlider/isSelected.js"></script>
+
 </body>
 </html>
--- js/flickity/genres.js
+++ js/flickity/genres.js
@@ -1,1 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let genresScroll = new Flickity('.all-genres-list-container', {
+        wrapAround: true,
+        prevNextButtons: false,
+        pageDots: false,
+    });
 
+    let isDragging = false;
+
+    genresScroll.on('dragStart', function (event, pointer) {
+        isDragging = true;
+        document.querySelector('.all-genres-list-container').classList.add('is-dragging');
+    });
+
+    genresScroll.on('pointerUp', function () {
+        setTimeout(function () {
+            isDragging = false;
+            document.querySelector('.all-genres-list-container').classList.remove('is-dragging');
+        }, 500);
+    });
+});
