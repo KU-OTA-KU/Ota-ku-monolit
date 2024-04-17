ee9d7e4b4a883cd93e161094e6fb026e1c510706
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-04-17 08:58:32 +0400

    баг когда при нажати на ссылку смена плеера кодика в плеере сайт обнавился
    Signed-off-by: Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>

--- src/components/player/player.php
+++ src/components/player/player.php
@@ -10,7 +10,7 @@
                     <button id="is-off"><i class="fa-solid fa-microphone"></i>Озвучка</button>
                 </div>
                 <div class="player-right-panel-content">
-                    <a href="">Kodik</a>
+                    <a href="javascript:void(0)">Kodik</a>
                 </div>
             </div>
         </div>
