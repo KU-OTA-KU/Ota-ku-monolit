7c909d75506b66d9674d936c3125e1aad31dfb1f
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-04-20 21:30:14 +0400

    исправлено Пользовательское соглашение

--- doc/terms_ru.php
+++ doc/terms_ru.php
@@ -5,7 +5,7 @@
   <link rel="stylesheet" href="../css/flickity.min.css">
   <meta charset="UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <title>OTA-KU Праваобладатели</title>
+  <title>OTA-KU Пользовательское соглашение</title>
   <link rel="shortcut icon" href="../assets/images/ota-ku-sign.ico" type="image/x-icon">
   <link rel="stylesheet" href="../src/_root/root.css"/>
   <link rel="stylesheet" href="../css/palette_dark.css"/>
